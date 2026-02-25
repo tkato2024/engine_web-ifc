e5e0d262d77afd1120601f05b91aec3841f1e37a
Takayuki Kato <t.kato@onestruction.com>
2026-02-25 12:06:28 +0900

    Clear stream state before token reads

--- src/cpp/web-ifc/parsing/IfcTokenStream.cpp
+++ src/cpp/web-ifc/parsing/IfcTokenStream.cpp
@@ -49,7 +49,12 @@ namespace webifc::parsing
 
   void IfcTokenStream::SetTokenSource(std::istream &requestData)
   { 
-     SetTokenSource([&](char* dest, size_t sourceOffset, size_t destSize) { requestData.seekg(sourceOffset); requestData.read(dest, destSize); return requestData.gcount();});
+     SetTokenSource([&](char* dest, size_t sourceOffset, size_t destSize) {
+       requestData.clear();
+       requestData.seekg(sourceOffset);
+       requestData.read(dest, destSize);
+       return requestData.gcount();
+     });
   }
   
   std::string_view IfcTokenStream::ReadString() 
